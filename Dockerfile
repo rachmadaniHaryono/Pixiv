55246ff4874b296a466b374c713c57772c70a6ee
KK <bebound@gmail.com>
2026-04-22 22:05:06 +0800

    Add translation fallback

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM python:3.14-slim
 
-RUN apt-get update && apt-get install -y --no-install-recommends locales && \
-    locale-gen en_US.UTF-8 && \
-    apt-get clean && rm -rf /var/lib/apt/lists/*
-ENV LC_ALL en_US.UTF-8
 WORKDIR /app
 COPY . .
 RUN pip install . --no-cache-dir && rm -rf *
--- src/pixivd/i18n.py
+++ src/pixivd/i18n.py
@@ -1,9 +1,10 @@
 import gettext
 import os
+
 import locale
 
-languages = [locale.getdefaultlocale()[0], 'en_US']
+languages = [locale.getlocale()[0], 'en_US']
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-t = gettext.translation('messages', os.path.join(current_path, "locale"), languages=languages)
+t = gettext.translation('messages', os.path.join(current_path, "locale"), languages=languages, fallback=True)
 i18n = t.gettext
